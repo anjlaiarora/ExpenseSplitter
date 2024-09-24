e693f070910763569d96ceebcdc1275ebf163b48
anjaliarora <anjaliaroraa908@gmail.com>
2024-09-24 13:43:21 +0530

    implement c#

---
+++ ExpenseSplitter.Server/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using ExpenseSplitter.Server.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace ExpenseSplitter.Server.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<UserModel> MongoClient;
+
+        public UserService(IMongoClient client)
+        {
+            var db = client.GetDatabase("Expensespitter");
+            var collection = db.GetCollection<UserModel>("Expense");
+            MongoClient = collection;
+        }
+
+        public async Task<UserModel> Create(UserModel user)
+        {
+            UserModel userModel = new UserModel();
+            if (user == null)
+            {
+                throw new ArgumentNullException("this user not exist");
+            }
+            else
+            {
+                var result = MongoClient.InsertOneAsync(user);
+
+            }
+            return userModel;
+        }
+    }
+}
