b667e79e2ddca29a23f17abe04aae83abcce89ea
mahi2103 <mjcode2103@gmail.com>
2024-09-27 12:07:07 +0530

    Add auth on backend

---
+++ ExpenseSplitter.Server/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using ExpenseSplitter.Server.Models;
+
+namespace ExpenseSplitter.Server.Services
+{
+    public interface IUserService
+    {
+        User GetUserByEmail(string email);
+        void CreateUser(User user);
+    }
+}
--- ExpenseSplitter.Server/Services/UserService.cs
+++ ExpenseSplitter.Server/Services/UserService.cs
@@ -5,45 +5,31 @@ using MongoDB.Driver;
 
 namespace ExpenseSplitter.Server.Services
 {
-    public class UserService
+    public class UserService: IUserService
     {
-        private readonly IMongoCollection<UserModel> MongoClient;
 
-        public UserService(IMongoClient client)
+        private readonly IMongoCollection<User> _users;
+
+        public UserService(IMongoClient config)
         {
-            var db = client.GetDatabase("Expensespitter");
-            var collection = db.GetCollection<UserModel>("Expense");
-            MongoClient = collection;
+            var database = config.GetDatabase("Expensespitter");
+            _users = database.GetCollection<User>("Users");
         }
 
-        //public async Task<UserModel> Create(UserModel user)
-        //{
-        //    UserModel userModel = new UserModel();
-        //    if (user == null)
-        //    {
-        //        throw new ArgumentNullException("this user not exist");
-        //    }
-        //    else
-        //    {
-        //        var result = MongoClient.InsertOneAsync(user);
+        public User GetUserByEmail(string email)
+        {
+            var filter = Builders<User>.Filter.Eq(user => user.Email, email);
+            return _users.Find(filter).FirstOrDefault();
+            //return _users.Find(user => user.Email == email).FirstOrDefault();
+        }
 
-        //    }
-        //    return userModel;
-        //}
-        public async Task<List<Models.UserModel>> GetAsync() =>
-            await MongoClient.Find(_ => true).ToListAsync();
+        public void CreateUser(User user)
+        {
+            _users.InsertOne(user);
+        }
 
-        public async Task<Models.UserModel?> GetAsync(string id) =>
-            await MongoClient.Find(x => x.UserName == id).FirstOrDefaultAsync();
 
-        public async Task CreateAsync(UserModel newUserModel) =>
-            await MongoClient.InsertOneAsync(newUserModel);
 
-        public async Task UpdateAsync(string id, UserModel updatedAll) =>
-            await MongoClient.ReplaceOneAsync(x => x.UserName == id, updatedAll);
-
-        public async Task RemoveAsync(string id) =>
-            await MongoClient.DeleteOneAsync(x => x.UserName == id);
 
     }
 }
