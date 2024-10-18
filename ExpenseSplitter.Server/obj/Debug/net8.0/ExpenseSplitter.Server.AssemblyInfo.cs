30f2595084825b42e6ebb55277b7036ee31e75aa
mahi2103 <mjcode2103@gmail.com>
2024-10-18 14:07:40 +0530

    splitter ui

--- ExpenseSplitter.Server/obj/Debug/net8.0/ExpenseSplitter.Server.AssemblyInfo.cs
+++ ExpenseSplitter.Server/obj/Debug/net8.0/ExpenseSplitter.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseSplitter.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5885e7a2b51ea19527b84237fd6797d37254b248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfcfa0e3d186bf9de1602ecc66ba641961e00d6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseSplitter.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseSplitter.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
